ddb9709505558cff57dd45910e2286477bc03882
Astella <elviszhao2016@126.com>
2019-11-15 11:22:00 +0800

    feat: add server hot reload using http.server Shutdown

--- cmd/main.go
+++ cmd/main.go
@@ -4,6 +4,10 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
+	"context"
+	"os"
+	"os/signal"
 
 	_ "Go-blog-server/internal/models"
 	"Go-blog-server/internal/routers"
@@ -21,5 +25,18 @@ func main() {
 		MaxHeaderBytes: 1 << 20,
 	}
 
-	log.Fatal(s.ListenAndServe())
+	quit := make(chan os.Signal)
+    signal.Notify(quit, os.Interrupt)
+    <- quit
+
+    log.Println("Shutdown Server ...")
+
+    ctx, cancel := context.WithTimeout(context.Background(), 5 * time.Second)
+    defer cancel()
+    if err := s.Shutdown(ctx); err != nil {
+        log.Fatal("Server Shutdown:", err)
+    }
+
+    log.Println("Server exiting")
+	
 }
